006652f50984ed6cf5c790083d671c5c14cb138f
Ward9250 <axolotlfan9250@gmail.com>
2016-09-19 21:31:59 +0100

    First basic draft of count_sites

--- src/var/mutation_counting/mutation_counting.jl
+++ src/var/mutation_counting/mutation_counting.jl
@@ -14,3 +14,30 @@ immutable Ambiguous <: SiteCase end
 immutable Pairdel <: SiteCase end
 
 include("bitwise_ops.jl")
+
+typealias FourBitAlphs Union{DNANucleotide{4},RNANucleotide{4}}
+
+function count_sites{T<:SiteCase,A<:FourBitAlphs}(::Type{T}, seq::BioSequence{A})
+    site_count = 0
+    bindex = Seq.bitindex(seq, 1)
+    off = Seq.offset(bindex)
+
+    # If the offset of the first element is not zero, then the first integer
+    # needs to be shifted / masked.
+    if off != 0
+        offsetint = seq.data[1] >> off
+        site_count += count_sites4(T, offsetint)
+    end
+
+    for i in 2:endof(seq.data)
+        site_count += count_sites4(T, seq.data[i])
+    end
+
+    return site_count
+end
+
+#=
+function count_sites{T<:SiteCase,A}(::Type{T}, a::BioSequence{A}, b::BioSequence{A})
+
+end
+=#
