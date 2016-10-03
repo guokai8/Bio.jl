d39321a143f3e7bd6015eeac4dc0c2deb9f4c45e
Ben J. Ward <axolotlfan9250@gmail.com>
2016-10-03 18:15:17 +0100

    Started integer alignment iterator

--- src/var/mutation_counting/mutation_counting.jl
+++ src/var/mutation_counting/mutation_counting.jl
@@ -13,6 +13,15 @@ immutable Gap <: SiteCase end
 immutable Ambiguous <: SiteCase end
 immutable Pairdel <: SiteCase end
 
+const OFFSET_MASK = [0xF,
+                     0xFF,
+                     0xFFF,
+                     0xFFFF,
+                     0xFFFFF,
+                     0xFFFFFF,
+                     0xFFFFFFF,
+                     0xFFFFFFFF]
+
 include("bitwise_ops.jl")
 
 typealias FourBitAlphs Union{DNAAlphabet{4},RNAAlphabet{4}}
@@ -38,8 +47,89 @@ function count_sites{A<:FourBitAlphs}(::Type{Ambiguous}, seq::BioSequence{A})
     return site_count
 end
 
-#=
+
+
+type AlignedIterator
+    seq::DNASequence
+end
+
+immutable AlignedIteratorState
+    at::UInt64
+    first::UInt64
+    last::UInt64
+    len::UInt64
+    firstoffset::UInt8
+    lastoffset::UInt8
+end
+
+function Base.start(iter::AlignedIterator)
+    return AlignedIteratorState(
+        1,
+        Seq.index(Seq.bitindex(iter.seq, 1)),
+        Seq.index(Seq.bitindex(iter.seq, endof(iter.seq))),
+        (Seq.index(Seq.bitindex(iter.seq, endof(iter.seq))) - Seq.index(Seq.bitindex(iter.seq, 1))) + 1,
+        Seq.offset(Seq.bitindex(iter.seq, 1)),
+        Seq.offset(Seq.bitindex(iter.seq, endof(iter.seq)))
+    )
+end
+
+function next(iter::AlignedIterator, state)
+    if state.firstoffset == 0 && state.lastoffset == 60
+        return (iter.seq.data[state.first + state.at], AlignedIteratorState(
+            state.at + 1,
+            state.first,
+            state.last,
+            state.len,
+            state.firstoffset,
+            state.lastoffset))
+    elseif state.firstoffset != 0 && state.lastoffset == 60
+
+    elseif state.firstoffset == 0 && state.lastoffset != 60
+
+    elseif state.firstoffset != 0 && state.lastoffset != 60
+
+    end
+end
+
+function done(iter, state)
+    return state.at > state.len
+end
+
+
 function count_sites{T<:SiteCase,A}(::Type{T}, a::BioSequence{A}, b::BioSequence{A})
+    @assert length(a) == length(b)
+
+    bindexA = Seq.bitindex(a, 1)
+    bindexB = Seq.bitindex(b, 1)
+
+
+
+    nelem_a = enda - starta
+    nelem_b = endb - startb
+    min_nelem = min(nelem_a, nelem_b)
+
+    start = max(starta, startb)
+    stop = min(enda, endb)
+
+    aligned_int_a = UInt64(0)
+    aligned_int_b = UInt64(0)
+
+    if Seq.offset(bindexA, 1) == 0 && Seq.offset(bindexB, 1) == 0
+        # Normal for loop
+    else
+
+        starta = Seq.index(bindexA)
+        startb = Seq.index(bindexB)
+        enda = Seq.index(Seq.bitindex(a, endof(a)))
+        endb = Seq.index(Seq.bitindex(b, endof(b)))
+
+        if Seq.offset(bindexA, 1) > Seq.offset(bindexB, 1)
+
+        end
+
+
+
+    end
+
 
 end
-=#
